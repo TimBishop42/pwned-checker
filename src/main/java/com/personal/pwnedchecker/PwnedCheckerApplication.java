693819097f6ff8321fe3970ee31c29f1a92f9631
bishoptim453@gmail.com <bishoptim453@gmail.com>
2021-03-04 22:00:37 +1100

    init commit

---
+++ src/main/java/com/personal/pwnedchecker/PwnedCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pwnedchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwnedCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwnedCheckerApplication.class, args);
+	}
+
+}
